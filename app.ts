ab9908f028b059c1bdf7963e037356d47a393cbf
Arun Kumar D <58033413+arunaji007@users.noreply.github.com>
2025-04-09 21:31:37 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -67,7 +67,7 @@ const StartServer = async () => {
     });
 
     app.get('/health', (req, res) => {
-        res.status(200).json({ status: 'OK check' });
+        res.status(200).json({ status: 'OK check test' });
     });
 
 }
