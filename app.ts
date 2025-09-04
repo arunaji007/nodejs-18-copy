8f735115ad99d65257687d0da4c3313697fc6df8
Arun Kumar D <58033413+arunaji007@users.noreply.github.com>
2025-09-04 15:34:18 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ const StartServer = async () => {
     app.use(express.json());
     
     app.listen(8080, () => {
-        console.log(`Listeningsss to port ${8080}`);
+        console.log(`Listeningss to port ${8080}`);
     }).on('error', (err) => {
         console.log('Error is', err);
         process.exit();
