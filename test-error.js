a199299a78acf81ab4f79231b16345b85c016fd0
Arun Kumar D <58033413+arunaji007@users.noreply.github.com>
2025-09-05 11:39:04 +0530

    Update test-error.js

--- test-error.js
+++ test-error.js
@@ -1,13 +1,1 @@
-class MyBase {
-  constructor() {
-    console.log("Base constructor");
-  }
-}
-
-class MyChild extends MyBase {
-  constructor() {
-    console.log("Child constructor"); // ❌ ESLint error: "constructor-super"
-  }
-}
-
-const c = new MyChild();
+console.log('test')
