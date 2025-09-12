40cb226ab5da450dbc9613fa3a5d135c82e9b405
Arun Kumar D <58033413+arunaji007@users.noreply.github.com>
2025-09-12 17:04:04 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -14,7 +14,7 @@ const StartServer = async () => {
 
   // console.error('Fatal error occurred');
   // process.exit(1);
-
+  console.log('test')
   const app = express();
   app.use(express.json());
 
